06c562e6e4c624a47b80074f99d5b0af3aaaf63a
Tommy Ho <tommyho.works@gmail.com>
2024-09-30 20:41:04 -0700

    change style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import Link from "next/link";
 import Button from "./components/Button";
 
 export default function Home() {
   return (
     <div className="container">
-      <h1>Hello world!</h1>
+      <header>
+        <h1>Hello world!</h1>
+      </header>
       <div className="card">
         <h2>Heading</h2>
-        <p>How are you doing?</p>
+        <p>Welcome to my website</p>
+        <Link href="/sample">Sample</Link>
         <section className="flex gap-4 mt-2">
           <Button type="button">Beggie</Button>
           <Button type="button" bgColor="secondary">
